5a97d5df4c6ebd0534bac773237e2d99d2411019
DM <opensource@hello.menu>
2024-04-20 11:32:53 +0100

    drop commonjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,25 +1,18 @@
 import { RollupOptions, OutputOptions } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+  import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
-const cjs: OutputOptions = {
+const es: OutputOptions = {
   dir: './dist',
-  entryFileNames: '[name].cjs',
+  entryFileNames: '[name].js',
   exports: 'named',
   externalLiveBindings: false,
-  format: 'cjs',
   freeze: false,
   // generatedCode: 'es2015',
   // interop: 'default',
   sourcemap: true,
-}
-
-const es: OutputOptions = {
-  ...cjs,
-  entryFileNames: '[name].mjs',
   format: 'es',
 };
 
@@ -33,7 +26,6 @@ export default [
     input: './src/index.ts',
     plugins: [
       nodeResolve({ preferBuiltins: true }),
-      commonjs(),
       json(),
       typescript({
         tsconfig: './tsconfig.build.json',
@@ -42,13 +34,12 @@ export default [
       }),
       terser(),
     ],
-    output: [ cjs, es ],
+    output: [ es ],
   },
   {
     input: './src/index.ts',
     plugins: [
       nodeResolve({ preferBuiltins: true }),
-      commonjs(),
       json(),
       typescript({
         tsconfig: './tsconfig.build.json',
@@ -59,10 +50,6 @@ export default [
       terser(),
     ],
     output: [
-      {
-        ...cjs,
-        ...preserveModules,
-      },
       {
         ...es,
         ...preserveModules,

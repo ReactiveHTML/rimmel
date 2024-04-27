5c29e614be15023948485fa78f57b6fd67a46a29
DM <opensource@hello.menu>
2024-04-27 19:34:57 +0100

    Revert "drop commonjs"
    This reverts commit 5a97d5df4c6ebd0534bac773237e2d99d2411019.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,25 @@
 import { RollupOptions, OutputOptions } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-  import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
-const es: OutputOptions = {
+const cjs: OutputOptions = {
   dir: './dist',
-  entryFileNames: '[name].js',
+  entryFileNames: '[name].cjs',
   exports: 'named',
   externalLiveBindings: false,
+  format: 'cjs',
   freeze: false,
   // generatedCode: 'es2015',
   // interop: 'default',
   sourcemap: true,
+}
+
+const es: OutputOptions = {
+  ...cjs,
+  entryFileNames: '[name].mjs',
   format: 'es',
 };
 
@@ -26,6 +33,7 @@ export default [
     input: './src/index.ts',
     plugins: [
       nodeResolve({ preferBuiltins: true }),
+      commonjs(),
       json(),
       typescript({
         tsconfig: './tsconfig.build.json',
@@ -34,12 +42,13 @@ export default [
       }),
       terser(),
     ],
-    output: [ es ],
+    output: [ cjs, es ],
   },
   {
     input: './src/index.ts',
     plugins: [
       nodeResolve({ preferBuiltins: true }),
+      commonjs(),
       json(),
       typescript({
         tsconfig: './tsconfig.build.json',
@@ -50,6 +59,10 @@ export default [
       terser(),
     ],
     output: [
+      {
+        ...cjs,
+        ...preserveModules,
+      },
       {
         ...es,
         ...preserveModules,

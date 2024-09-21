d139d908615ae89c446ab52d204a9e20b865c8c8
DM <opensource@hello.menu>
2024-09-21 22:08:57 +0100

    optionally curried dataset source

--- src/sources/dataset-source.ts
+++ src/sources/dataset-source.ts
@@ -1,7 +1,9 @@
 import type { RMLTemplateExpressions } from "../types/internal";
+import { EventListenerFunction } from "../types/dom";
+import type { Observer } from "../types/futures";
 
 import { map } from 'rxjs';
-import { pipeIn } from '../utils/input-pipe';
+import { inputPipe } from '../utils/input-pipe';
 import { Source } from "../types/source";
 
 /**
@@ -13,11 +15,27 @@ import { Source } from "../types/source";
  * @example <button data-foo="bar" onclick="${Dataset(resolveFn, 'foo')}"> ... </button>
  * @example <button data-foo="bar" onclick="${Dataset(rejectFn, 'foo')}"> ... </button>
 **/
-export const Dataset = <T extends HTMLElement, I extends Event, O extends RMLTemplateExpressions.SourceExpression<string | undefined>>(key: string): Source<I, O> =>
-	pipeIn<I, O>(
-		map((e: I) => <O>((<T>e.target).dataset[key]))
-	)
-;
+export const Dataset = <T extends HTMLElement, I extends Event, O extends RMLTemplateExpressions.SourceExpression<string | undefined>>
+	(key: string, source?: RMLTemplateExpressions.SourceExpression<I | O>): Source<I, O> | Observer<I> | EventListenerFunction<I> => {
+		const handler = inputPipe<I, O>(
+			map((e: I) => <O>((<T>e.target).dataset[key]))
+		);
+
+		return (
+			source
+			? handler(source)
+			: handler
+		);
+};
+
+// export const Dataset2 = <T extends HTMLElement, I extends Event, O extends RMLTemplateExpressions.SourceExpression<string | undefined>>
+// 	(key: string): Source<I, O> =>
+// 		inputPipe<I, O>(
+// 			map((e: I) => {
+// 				return <O>((<T>e.target).dataset[key])
+// 			})
+// 		)
+// ;
 
 /**
  * An Event Source emitting a numerical dataset value from the underlying element instead of a regular DOM Event object
@@ -29,7 +47,7 @@ export const Dataset = <T extends HTMLElement, I extends Event, O extends RMLTem
  * @example <button data-foo="123" onclick="${Numberset(rejectFn, 'foo')}"> ... </button>
 **/
 export const Numberset = <T extends HTMLElement, I extends Event, O extends RMLTemplateExpressions.SourceExpression<number>>(key: string): Source<I, O> =>
-	pipeIn<I, O>(
+	inputPipe<I, O>(
 		map((e: Event) => <O>Number((<T>e.target).dataset[key]))
 	)
 ;

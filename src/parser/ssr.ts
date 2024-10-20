6eebc3630a4133b7c545989fc555b96574879662
DM <opensource@hello.menu>
2024-10-20 09:04:39 +0100

    ssr

--- src/parser/ssr.ts
+++ src/parser/ssr.ts
@@ -11,64 +11,67 @@ import { from, Observable, of } from 'rxjs';
 let count = 0;
 
 const hydrateFunction = `
-    <script>
-        const no_sink = (name) => (key, data) => console.error('[Rimmel]: called unknown hydration sink "\${name}"'),
-        sinks = {
-            InnerHTML: (key, data) => document.querySelector(\`[RESOLVE="\${key}"]\`).innerHTML = data,
-            AppendHTML: (key, data) => document.querySelector(\`[RESOLVE="\${key}"]\`).innerHTML += data,
-            Style: (key, data) => Object.entries(data).forEach(([k, v]) => {
-                document.querySelector(\`[RESOLVE="\${key}"]\`).style[k] = v;
-            }),
-            Class: (key, data) => Object.entries(data).forEach(([k, v]) => {
-                document.querySelector(\`[RESOLVE="\${key}"]\`).classList.toggle(k, v);
-            }),
-        };
-        Rimmel_Hydrate = (data) => {
-            const [key, handler] = data;
-            console.log('SINK type', handler.t, handler.value);
-            (sinks[handler.t] ?? no_sink(handler.sink))(key, handler.value);
-        }
-    </script>
+<script>
+	const resolvables = new Map([...document.querySelectorAll('[RESOLVE]')].map(n=>[n.getAttribute('resolve'), n]));
+
+	const no_sink = (name) => (node, data) => console.error(\`[Rimmel]: called unknown hydration sink "\${name}"\`);
+	const sinks = {
+		Attribute: (node, data) => Object.entries(data).forEach(([k, v]) => {
+			k == 'class' ? node.classList.add(v) : node[k] = v;
+		}),
+		InnerHTML: (node, data) => node.innerHTML = data,
+		AppendHTML: (node, data) => node.inertAdjacentHTML('beforeend', data),
+		class: (node, data) => Object.entries(data).forEach(([k, v]) => node.classList.toggle(k, v)),
+		Style: (node, data) => Object.entries(data).forEach(([k, v]) => node.style[k] = v),
+		StyleObject: (node, data) => Object.entries(data).forEach(([k, v]) => node.style[k] = v),
+	};
+
+	function Rimmel_Hydrate(data) {
+		const [key, conf] = data;
+		const node = resolvables.get(key);
+		console.log('SINK type', conf.t, conf.value);
+		(sinks[conf.t] ?? no_sink(conf.sink))(node, conf.value);
+	}
+</script>
 `;
 
 export const rml = (strings: TemplateStringsArray, ...args: RMLTemplateExpression[]): Observable<HTMLString> => {
-    debugger;
-    const hydrationCall = (data: string) => <HTMLString>`\n<script>Rimmel_Hydrate(${data});</script>`;
+	const hydrationCall = (data: string) => <HTMLString>`\n<script>Rimmel_Hydrate(${data});</script>`;
 
-    const str: HTMLString = <HTMLString>(parser(strings, ...args) +hydrateFunction);
+	const str: HTMLString = <HTMLString>(parser(strings, ...args) +hydrateFunction);
 
-    const tasks = [...waitingElementHanlders.entries()]
-        .flatMap(([key, jobs]) =>
-            jobs.map(async (job: SinkBindingConfiguration<Element> | any) => {
+	const tasks = [...waitingElementHanlders.entries()]
+		.flatMap(([key, jobs]) =>
+			 jobs.map(async (job: SinkBindingConfiguration<Element> | any) => {
 
-              const res = await job.source;
-              console.log('>>> JOB', res);
-              return [
-                  key,
-                  {
-                      resolved: 'ssr',
-                      ...job,
-                      handler: await job.handler,
-                      value: res,
-                      sink: job.t,
-                      count: (count++),
-                  }
-              ]
-            })
-            .map(p => from(p))
-        )
-    ;
+				 const res = await job.source;
+				 // console.log('>>> JOB', res);
+				 return [
+					 key,
+					 {
+						 resolved: 'ssr',
+						 ...job,
+						 handler: await job.handler,
+						 value: res,
+						 sink: job.t,
+						 count: (count++),
+					 }
+				 ]
+			 })
+			 .map(p => from(p))
+		)
+	;
 
-    const asyncStuff = () => of(...tasks).pipe(
-        mergeAll(),
-        tap(x => console.log('>>>', JSON.stringify(x))),
-        map(x => hydrationCall(JSON.stringify(x)))
-    );
+	const asyncStuff = () => of(...tasks).pipe(
+		mergeAll(),
+		// tap(x => console.log('>>>', x)),
+		map(x => hydrationCall(JSON.stringify(x)))
+	);
 
-    return of(str).pipe(
-        mergeWith(asyncStuff()),
-        endWith(<HTMLString>'\n<!-- streams end -->\n</body>\n</html>'),
-        tap(() => waitingElementHanlders.clear()),
-    );
+	return of(str).pipe(
+		mergeWith(asyncStuff()),
+		endWith(<HTMLString>'\n<!-- streams end -->\n</body>\n</html>'),
+		tap(() => waitingElementHanlders.clear()),
+	);
 };
 

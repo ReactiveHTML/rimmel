becb79c267dbb25082bf5543605fdd1b15d380d7
DM <opensource@hello.menu>
2025-04-02 19:25:25 +0100

    fix native observable extension

--- src/types/monkey-patched-observable.ts
+++ src/types/monkey-patched-observable.ts
@@ -1,4 +1,5 @@
-export class MonkeyPatchedObservable<T> extends Observable<T> {
+const hasNativeObservable = typeof Observable === 'function';
+export class MonkeyPatchedObservable<T> extends (hasNativeObservable ? Observable : Object)<T> {
   // static [operatorName: (string | symbol) & Exclude<string | symbol, keyof Observable>]: <R=T>(...args: any[]) => MonkeyPatchedObservable<R>;// <T, O extends MonkeyPatchedObservable<T>>() => MonkeyPatchedObservable<O>;
   static [key: string]: <R>(...args: any[]) => MonkeyPatchedObservable<R>;
   /* @ts-ignore */

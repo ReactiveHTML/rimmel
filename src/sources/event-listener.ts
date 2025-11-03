ea938a35d577cd609c32a64b2708524ef5a49f50
sshekhar563 <shekharsiddhant93@gmail.com>
2025-11-03 17:42:10 +0530

    refactor: use addListener() in eventListnerSource and EventHandlerSink (#61)

--- src/sources/event-listener.ts
+++ src/sources/event-listener.ts
@@ -1,9 +1,11 @@
-import { HTMLEventName } from "../types/dom"
+import { addListener } from "../lib/addListener";
+import type { RMLEventListener, HTMLEventName } from "../types/dom";
 
-const options = { }; // TODO: define better
-
-// TODO: use addListener() instead of this
-export const eventListnerSource =
-	(node: HTMLElement, eventName: HTMLEventName, handler: EventListenerOrEventListenerObject) =>
-		node.addEventListener(eventName, handler, options)
-;
+export const eventListnerSource = (
+  node: HTMLElement,
+  eventName: HTMLEventName,
+  handler: RMLEventListener,
+  options?: AddEventListenerOptions
+) => {
+  return addListener(node, eventName, handler, options);
+};

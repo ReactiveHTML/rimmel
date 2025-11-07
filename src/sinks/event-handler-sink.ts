fa8c2265b02c5d5a05bdd49b41f36b8f84b93c8b
Siddhant Shekhar <shekharsiddhant93@gmail.com>
2025-11-07 17:06:52 +0530

    refactor: use addListener() in eventListnerSource and EventHandlerSink (#61) (#69)

--- src/sinks/event-handler-sink.ts
+++ src/sinks/event-handler-sink.ts
@@ -1,11 +1,7 @@
-import type { HTMLEventName } from "../types/dom";
+import { addListener } from "../lib/addListener";
+import type { RMLEventListener, HTMLEventName } from "../types/dom";
 import type { Sink } from "../types/sink";
 
 export const EventHandlerSink: Sink<HTMLElement> = (node: HTMLElement, e: HTMLEventName) =>
-	// FIXME: wrap h and options in an object, as nothing will emit both values!
-	// TODO: use addListener() instead of this
-	(h: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) => {
-		node.addEventListener(e, h, options);
-		// ? node.removeEventListener(e, h) // FIXME: to be able to remove an event listener, will need to store a reference to it first!
-		// : node.addEventListener(e, h)
-	};
+    (handler: RMLEventListener, options?: AddEventListenerOptions) =>
+        addListener(node, e, handler, options);
